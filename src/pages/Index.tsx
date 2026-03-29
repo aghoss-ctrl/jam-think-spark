1c431e8dfa20a9b5e33c9dece138dad1aae08d20
aghoss-ctrl <aghoss@gmail.com>
2026-03-29 09:26:04 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,7 +17,7 @@ const Index = () => {
             height={1080}
           />
         </div>
-      <div className="font-bold text-purple-600 mt-[100px]">
+      <div className="font-bold text-purple-600 mt-[30px]">
         <p className="text-xl md:text-2xl text-hero-foreground/80 max-w-2xl">
      View this page to know what and why you need to register for this unique community. 
     Click the Start Here tab above for how to navigate. 
